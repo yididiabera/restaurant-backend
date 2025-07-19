7d28a07b529877da8cdfb2c00b0c3bdaee9d74bf
Jedidiah Abera <yididiabera87@gmail.com>
2025-07-19 18:39:53 +0300

    Clean directory

--- config/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
-};
-
-module.exports = generateToken;
